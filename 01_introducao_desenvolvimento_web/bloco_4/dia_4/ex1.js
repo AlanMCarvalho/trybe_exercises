3b5236906c5fc647c1dc67de6434adf73af8a80f
Alan Carvalho <alan.fate@gmail.com>
2020-09-01 16:08:18 -0300

    resolução ex1.

---
+++ 01_introducao_desenvolvimento_web/bloco_4/dia_4/ex1.js
@@ -0,0 +1,24 @@
+let a = 2;
+let b = 5;
+
+function sum(a, b){
+    return a + b;
+}
+
+function subtraction(b, a){
+    return b - a;
+}
+
+function multiplication(a, b){
+    return a * b;
+}
+
+function division(b, a){
+    return b / a;
+}
+
+function module(b, a){
+    return b ** a;
+}
+
+
